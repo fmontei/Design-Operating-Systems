f4309f201cb72d651ed68912e16174540c203379
ywu342 <valence931215@gatech.edu>
2015-09-27 01:20:40 -0400

    a rogue program

---
+++ rogue.c
@@ -0,0 +1,31 @@
+#include <fcntl.h>         /* defines options flags */
+#include <sys/types.h>     /* defines types used by sys/stat.h */
+#include <sys/stat.h>      /* defines S_IREAD & S_IWRITE  */
+
+static char message[] = "Something";
+
+int main()
+{
+int fd;
+char buffer[80];
+
+fd = open("test.dat",O_RDWR | O_CREAT | O_EXCL, S_IREAD | S_IWRITE);
+if (fd != -1)
+   {
+   //printf("test.dat opened for read/write access\n");
+   int i=0;
+   for (;i<100000000;i++){
+   	write(fd, message, sizeof(message));
+   }
+   //lseek(fd, 0L, 0);     /* go back to the beginning of the file */
+/*   
+   if (read(fd, buffer, sizeof(message)) == sizeof(message))
+      printf("\"%s\" was written to test.dat\n", buffer);
+   else
+      printf("*** error reading test.dat ***\n");*/
+   close (fd);
+   }
+else
+   printf("*** test.dat already exists ***\n");
+exit (0);
+}
