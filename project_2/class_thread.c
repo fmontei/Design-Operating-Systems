450d49d8b1a59df2f9597a0e74232cf50635e5fa
Felipe Monteiro <fmontei@g.clemson.edu>
2015-11-13 02:02:29 +0000

    Removed pthread_t from params.

--- project_2/class_thread.c
+++ project_2/class_thread.c
@@ -9,19 +9,19 @@ static pthread_cond_t count_cond;
 
 typedef struct {
   int count;
-  pthread_t thread_id;
+  int thread_id;
 } pthread_monitor;
 
 static pthread_monitor thread_monitor[NUM_THREAD];
 
-void nodeadlock_init(pthread_t thread_id, int index) {
+void nodeadlock_init(int thread_id, int index) {
   if (index == 0 || index == 1) {
     thread_monitor[index].count = 0;
     thread_monitor[index].thread_id = thread_id;
   }
 }
 
-pthread_monitor *get_thread_by_id(pthread_t thread_id) {
+pthread_monitor *get_thread_by_id(int thread_id) {
   int i;
   for (i = 0; i < NUM_THREAD; i++) {
     if (thread_monitor[i].thread_id == thread_id) {
@@ -31,7 +31,7 @@ pthread_monitor *get_thread_by_id(pthread_t thread_id) {
   return NULL;
 }
 
-pthread_monitor *get_other_thread_by_id(pthread_t thread_id) {
+pthread_monitor *get_other_thread_by_id(int thread_id) {
   int i;
   unsigned int found = FALSE;
   for (i = 0; i < NUM_THREAD; i++) {
@@ -46,7 +46,7 @@ pthread_monitor *get_other_thread_by_id(pthread_t thread_id) {
   else return NULL;
 }
 
-void nodeadlock_mutex_lock(pthread_t this_thread_id) {
+void nodeadlock_mutex_lock(int this_thread_id) {
   pthread_mutex_lock(&count_mutex); // Lock 
   
   pthread_monitor *this_monitor = get_thread_by_id(this_thread_id);
@@ -73,7 +73,7 @@ void nodeadlock_mutex_lock(pthread_t this_thread_id) {
   pthread_mutex_unlock(&count_mutex); // Unlock
 }
 
-void nodeadlock_mutex_unlock(pthread_t this_thread_id) {
+void nodeadlock_mutex_unlock(int this_thread_id) {
   pthread_mutex_lock(&count_mutex); // Lock 
   
   pthread_monitor *this_monitor = get_thread_by_id(this_thread_id);
@@ -97,13 +97,13 @@ void nodeadlock_mutex_unlock(pthread_t this_thread_id) {
   pthread_mutex_unlock(&count_mutex); // Unlock
 }
 
-void nodeadlock(char *action, pthread_t thread_id, int index) {
+void nodeadlock(char *action, int  *thread_id, int index) {
   if (strcmp(action, "init") == 0) {
-    nodeadlock_init(thread_id, index);
+    nodeadlock_init(*thread_id, index);
   } else if (strcmp(action, "lock") == 0) {
-    nodeadlock_mutex_lock(thread_id);
+    nodeadlock_mutex_lock(*thread_id);
   } else if (strcmp(action, "unlock") == 0) {
-    nodeadlock_mutex_unlock(thread_id);
+    nodeadlock_mutex_unlock(*thread_id);
   } 
 }
 
@@ -179,7 +179,7 @@ int class_mutex_lock(class_mutex_ptr cmutex)
   pthread_t thread_id = pthread_self();
   printf("THREAD ID (LOCK) = %d\n", (int) thread_id);
 
-  nodeadlock("lock", thread_id, -1 /* Unusued arg */);
+  nodeadlock("lock", (int *) &thread_id, -1 /* Unusued arg */);
   
   if(pthread_mutex_lock(&cmutex->mutex))
   {
@@ -196,7 +196,7 @@ int class_mutex_unlock(class_mutex_ptr cmutex)
   pthread_t thread_id = pthread_self();
   printf("THREAD ID (UNLOCK) = %d\n", (int) thread_id);
   
-  nodeadlock("unlock", thread_id, -1 /* Unusued arg */);
+  nodeadlock("unlock", (int *) &thread_id, -1 /* Unusued arg */);
   
   if(pthread_mutex_unlock(&cmutex->mutex))
   {
@@ -221,7 +221,7 @@ int class_thread_create(class_thread_t * cthread, void *(*start)(void *), void *
   //Hacking a bit to get everything working correctly
   memcpy(cthread, &temp_pthread, sizeof(pthread_t));
   printf("THREAD ID CREATE = %d\n", (int) temp_pthread);
-  nodeadlock("init", temp_pthread, index++);
+  nodeadlock("init", (int *) &temp_pthread, index++);
 
   return 0;
 }
