b5b9c0941d8b9748c6614a17881af2186f830cec
Felipe Monteiro <fmontei@users.noreply.github.com>
2015-11-13 21:35:50 -0500

    Alternative to pointers (somehow crashing)

--- project_2/nodeadlock.c
+++ project_2/nodeadlock.c
@@ -10,7 +10,6 @@ static DECLARE_WAIT_QUEUE_HEAD(wq);
 typedef struct {
   int count;
   int thread_id;
-  int valid;
 } pthread_monitor;
 
 static pthread_monitor thread_monitor[2];
@@ -22,7 +21,6 @@ long nodeadlock_init(int thread_id, int index) {
   if (index == 0 || index == 1) {
     thread_monitor[index].count = 0;
     thread_monitor[index].thread_id = thread_id;
-    thread_monitor[index].valid = 1;
     printk(KERN_INFO "nodeadlock_init called with thread_id = %d, index = %d\n",
       thread_id, index);
     return 0;
@@ -30,20 +28,17 @@ long nodeadlock_init(int thread_id, int index) {
   return -1;
 }
 
-pthread_monitor get_thread_by_id(int thread_id) {
-  pthread_monitor invalid;
+int get_thread_by_id(int thread_id) {
   int i;
   for (i = 0; i < 2; i++) {
     if (thread_monitor[i].thread_id == thread_id) {
-      return thread_monitor[i];
+      return i;
     }
   }
-  invalid.valid = 0;
-  return invalid;
+  return -1;
 }
 
-pthread_monitor get_other_thread_by_id(int thread_id) {
-  pthread_monitor invalid;
+int get_other_thread_by_id(int thread_id) {
   int i;
   unsigned int found = 0;
   for (i = 0; i < 2; i++) {
@@ -54,66 +49,64 @@ pthread_monitor get_other_thread_by_id(int thread_id) {
   }
   
   if (found == 1 && i == 0) {
-    return thread_monitor[1];
+    return 1;
   } else if (found == 1 && i == 1) {
-    return thread_monitor[0];
+    return 0;
   } else {
-    invalid.valid = 0;
-    return invalid;
+    return -1;
   }
 }
 
 long nodeadlock_mutex_lock(int this_thread_id) {
-  pthread_monitor this_monitor, other_monitor;
+  int this_monitor_index, other_monitor_index;
 
   mutex_trylock(&count_mutex); // Lock 
   
-  this_monitor = get_thread_by_id(this_thread_id);
-  other_monitor = get_other_thread_by_id(this_thread_id);
-  
-  if (this_monitor.valid == 0 || other_monitor.valid == 0) {
+  this_monitor_index = get_thread_by_id(this_thread_id);
+  other_monitor_index = get_other_thread_by_id(this_thread_id);
+
+  if (this_monitor_index == -1 || other_monitor_index == -1) {
     // printk(KERN_INFO "ERROR: one of the monitors is NULL\n");
     mutex_unlock(&count_mutex); // Unlock
     return -1;
-  }   
+  } 
   
-  while (other_monitor.count > 0) {
+  while (thread_monitor[other_monitor_index].count > 0) {
     // pthread_cond_wait(&count_cond, &count_mutex);
-    wait_event_interruptible(wq, (other_monitor.count > 0));    
-    other_monitor = get_other_thread_by_id(this_thread_id);
+    // wait_event_interruptible(wq, (other_monitor->count > 0));    
+    other_monitor_index = get_other_thread_by_id(this_thread_id);
   
-    if (other_monitor.valid == 0) {
+    if (other_monitor_index == -1) {
       // printk(KERN_INFO "ERROR: one of the monitors is NULL\n");
       mutex_unlock(&count_mutex); // Unlock
       return -1;
     }  
   }
   
-  this_monitor.count += 1;
+  thread_monitor[this_monitor_index].count += 1;
     
   mutex_unlock(&count_mutex); // Unlock
   return 0;
 }
 
 long nodeadlock_mutex_unlock(int this_thread_id) {
-  pthread_monitor this_monitor, other_monitor;  
+  int this_monitor_index;
 
   mutex_trylock(&count_mutex); // Lock 
   
-  this_monitor = get_thread_by_id(this_thread_id);
-  other_monitor = get_other_thread_by_id(this_thread_id);
+  this_monitor_index = get_thread_by_id(this_thread_id);
   
-  if (this_monitor.valid == 0 || other_monitor.valid == 0) {
+  if (this_monitor_index == -1) {
     // printk(KERN_INFO "ERROR: one of the monitors is NULL\n");
     mutex_unlock(&count_mutex); // Unlock
     return -1;
-  }
+  } 
   
-  this_monitor.count -= 1;
+  thread_monitor[this_monitor_index].count -= 1;
   
-  if (this_monitor.count == 0) {
+  if (thread_monitor[this_monitor_index].count == 0) {
     // pthread_cond_signal(&count_cond);
-    wake_up_interruptible(&wq);
+    // wake_up_interruptible(&wq);
   }
     
   mutex_unlock(&count_mutex); // Unlock
