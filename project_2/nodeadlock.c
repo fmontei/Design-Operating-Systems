2de3cae9ab414ee8a3a999fb89555110c562ecf9
Felipe Monteiro <fmontei@users.noreply.github.com>
2015-11-13 20:33:48 -0500

    Remove pointers

--- project_2/nodeadlock.c
+++ project_2/nodeadlock.c
@@ -10,6 +10,7 @@ static DECLARE_WAIT_QUEUE_HEAD(wq);
 typedef struct {
   int count;
   int thread_id;
+  int valid;
 } pthread_monitor;
 
 static pthread_monitor thread_monitor[2];
@@ -21,22 +22,26 @@ void nodeadlock_init(int thread_id, int index) {
   if (index == 0 || index == 1) {
     thread_monitor[index].count = 0;
     thread_monitor[index].thread_id = thread_id;
+    thread_monitor[index].valid = 1;
     printk(KERN_INFO "nodeadlock_init called with thread_id = %d, index = %d\n",
       thread_id, index);
   }
 }
 
-pthread_monitor *get_thread_by_id(int thread_id) {
+pthread_monitor get_thread_by_id(int thread_id) {
+  pthread_monitor invalid;
   int i;
   for (i = 0; i < 2; i++) {
     if (thread_monitor[i].thread_id == thread_id) {
-      return &thread_monitor[i];
+      return thread_monitor[i];
     }
   }
-  return NULL;
+  invalid.valid = 0;
+  return invalid;
 }
 
-pthread_monitor *get_other_thread_by_id(int thread_id) {
+pthread_monitor get_other_thread_by_id(int thread_id) {
+  pthread_monitor invalid;
   int i;
   unsigned int found = 0;
   for (i = 0; i < 2; i++) {
@@ -46,59 +51,64 @@ pthread_monitor *get_other_thread_by_id(int thread_id) {
     }
   }
   
-  if (found == 1 && i == 0) return &thread_monitor[1];
-  else if (found == 1 && i == 1) return &thread_monitor[0];  
-  else return NULL;
+  if (found == 1 && i == 0) {
+    return thread_monitor[1];
+  } else if (found == 1 && i == 1) {
+    return thread_monitor[0];
+  } else {
+    invalid.valid = 0;
+    return invalid;
+  }
 }
 
 void nodeadlock_mutex_lock(int this_thread_id) {
-  pthread_monitor *this_monitor, *other_monitor;
+  pthread_monitor this_monitor, other_monitor;
 
   mutex_trylock(&count_mutex); // Lock 
   
   this_monitor = get_thread_by_id(this_thread_id);
   other_monitor = get_other_thread_by_id(this_thread_id);
   
-  if (this_monitor == NULL || other_monitor == NULL) {
+  if (this_monitor.valid == 0 || other_monitor.valid == 0) {
     // printk(KERN_INFO "ERROR: one of the monitors is NULL\n");
-    mutex_unlock(&count_mutex); // Unlock
-    return;
+    // mutex_unlock(&count_mutex); // Unlock
+    // return;
   }   
   
-  while (other_monitor->count > 0) {
+  while (other_monitor.count > 0) {
     // pthread_cond_wait(&count_cond, &count_mutex);
-    // wait_event_interruptible(wq, other_monitor->count > 0);    
+    // wait_event_interruptible(wq, other_monitor.count > 0);    
     other_monitor = get_other_thread_by_id(this_thread_id);
   
-    if (this_monitor == NULL || other_monitor == NULL) {
+    if (other_monitor.valid == 0) {
       // printk(KERN_INFO "ERROR: one of the monitors is NULL\n");
-      mutex_unlock(&count_mutex); // Unlock
-      return;
+      // mutex_unlock(&count_mutex); // Unlock
+      // return;
     }  
   }
   
-  this_monitor->count += 1;
+  this_monitor.count += 1;
     
   mutex_unlock(&count_mutex); // Unlock
 }
 
 void nodeadlock_mutex_unlock(int this_thread_id) {
-  pthread_monitor *this_monitor, *other_monitor;  
+  pthread_monitor this_monitor, other_monitor;  
 
   mutex_trylock(&count_mutex); // Lock 
   
   this_monitor = get_thread_by_id(this_thread_id);
   other_monitor = get_other_thread_by_id(this_thread_id);
   
-  if (this_monitor == NULL || other_monitor == NULL) {
+  if (this_monitor.valid == 0 || other_monitor.valid == 0) {
     // printk(KERN_INFO "ERROR: one of the monitors is NULL\n");
-    mutex_unlock(&count_mutex); // Unlock
-    return;
+    // mutex_unlock(&count_mutex); // Unlock
+    // return;
   }
   
-  this_monitor->count -= 1;
+  this_monitor.count -= 1;
   
-  if (this_monitor->count == 0) {
+  if (this_monitor.count == 0) {
     // pthread_cond_signal(&count_cond);
     // wake_up_interruptible(&wq);
   }
