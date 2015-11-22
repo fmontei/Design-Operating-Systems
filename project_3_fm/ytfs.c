76e175e4ad04849684adc378d176b94b3df32f61
Felipe Monteiro <fmontei@g.clemson.edu>
2015-11-22 21:04:03 +0000

    Figured out how to create subdirectories (for music categories.

--- project_3_fm/ytfs.c
+++ project_3_fm/ytfs.c
@@ -1,5 +1,3 @@
-
-
 /*
   FUSE: Filesystem in Userspace
   Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
@@ -11,31 +9,34 @@
 */
 
 #define FUSE_USE_VERSION 26
+#define NUM_CATEGORY 4
 
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <stdbool.h>
 
 static const char *hello_str = "Hello World!\n";
 static const char *hello_path = "/hello";
 static const char *bye_path = "/bye";
+
+static const char *CATEGORY_DIRS[NUM_CATEGORY] = {"/Album", "/Artist", "/Genre", "/Year"};
+
+bool is_directory(const char *path);
 
 static int hello_getattr(const char *path, struct stat *stbuf)
 {
 	int res = 0;
 
 	memset(stbuf, 0, sizeof(struct stat));
-	if (strcmp(path, "/") == 0) {
+	if (is_directory(path)) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
-	} else if (strcmp(path, hello_path) == 0 || strcmp(path, bye_path) == 0) {
-		stbuf->st_mode = S_IFREG | 0444;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(hello_str);
-	} else
+	} else {
 		res = -ENOENT;
+    }
 
 	return res;
 }
@@ -45,14 +46,18 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 {
 	(void) offset;
 	(void) fi;
+    int i;
 
-	if (strcmp(path, "/") != 0)
+	if (!is_directory(path))
 		return -ENOENT;
 
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
-	filler(buf, hello_path + 1, NULL, 0);
-    filler(buf, bye_path + 1, NULL, 0);
+    if (strcmp(path, "/") == 0) {
+        for (i = 0; i < NUM_CATEGORY; i++) {
+            filler(buf, CATEGORY_DIRS[i] + 1, NULL, 0);
+        }
+    }
 
 	return 0;
 }
@@ -99,3 +104,21 @@ int main(int argc, char *argv[])
 	return fuse_main(argc, argv, &hello_oper, NULL);
 }
 
+bool is_directory(const char *path)
+{
+    bool is_dir = false;
+    int i;
+    
+    if (strcmp(path, "/") == 0) {
+        is_dir = true;
+    }
+
+    for (i = 0; i < NUM_CATEGORY; i++) {
+        if (strcmp(path, CATEGORY_DIRS[i]) == 0) {
+            is_dir = true;
+            break;
+        }
+    }
+
+    return is_dir;
+}
