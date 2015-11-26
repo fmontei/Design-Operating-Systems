893d4b475ae320aa5bef91c0417efd6b1089d5b4
Felipe Monteiro <fmontei@g.clemson.edu>
2015-11-26 19:38:30 +0000

    Updated Python script to work with .env file. Removed unused/junk files.

--- project_3_fm/ytfs.py
+++ project_3_fm/ytfs.py
@@ -5,8 +5,11 @@ BUCKET_NAME = 'ytfs-felipe-yaling-garrett'
 DB_NAME = 'ytfs.db'
 TABLE_NAME = 'mp3'
 
-def run(download = None, 
-        upload = None):
+def run(root_dir,
+        mount_dir,
+        search_dir,
+        download_flg, 
+        upload_flg):
         
     errors = []
     file_or_dir = {}
@@ -15,35 +18,22 @@ def run(download = None,
     if conn is None:
         return False, error
     
-    if download:
+    if download_flg:
         file_or_dir['action'] = 'download'
-        file_or_dir['path'] = download
-        if not isdir(download):
-            return False, 'Download folder is invalid.'
-    elif upload:
+        file_or_dir['path'] = root_dir
+    elif upload_flg:
         file_or_dir['action'] = 'upload'
-        file_or_dir['path'] = upload
-        if isfile(upload):
-            file_or_dir['type'] = 'file'
-        elif isdir(upload):
-            file_or_dir['type'] = 'dir'
-        else:
-            return False, 'Upload file/folder is invalid.'
+        file_or_dir['path'] = root_dir
           
-    if download:
+    if download_flg:
         download_files(file_or_dir)
-        files_to_process = get_files_from_dir(file_or_dir['path'], 
-            is_dir = True) 
-        errors = parse_header_info(conn = conn, files = files_to_process)
-    elif upload:           
+        files_to_process = get_files_from_dir(file_or_dir['path']) 
+    elif upload_flg:           
         files_to_process = []
-        if file_or_dir['type'] == 'dir':
-            files_to_process = get_files_from_dir(file_or_dir['path'], 
-                is_dir = True) 
-        elif file_or_dir['type'] == 'file':
-            files_to_process = get_files_from_dir(file_or_dir['path'],
-                is_dir = False)          
+        files_to_process = get_files_from_dir(file_or_dir['path'])         
         upload_files(files_to_process, file_or_dir)
+
+    errors = parse_header_info(conn = conn, files = files_to_process)
         
     return True, errors
 
@@ -59,33 +49,26 @@ def init_db():
             '(id integer primary key autoincrement, file text, title text, '\
             'artist text, album text, genre text, '\
             'year text);'.format(table = TABLE_NAME))
-        """
         cursor.execute('CREATE UNIQUE INDEX ukey ON {table} '\
             '(file, title, artist, album, genre, year);'\
             .format(table = TABLE_NAME))
-        """
     except sqlite3.OperationalError as e:
         error = 'Error initializing DB. Error: {e}.'.format(e = e)
 
     return True, error, conn
 
-def get_files_from_dir(file_or_dir, is_dir = False):
+def get_files_from_dir(file_or_dir):
     from os import walk
     from os.path import abspath, dirname
     
     files_to_process = []
     
-    if is_dir:
-        for (dirpath, dirnames, filenames) in walk(file_or_dir):
-            for file_name in filenames:
-                complete_path = abspath(dirpath) + '/'
-                complete_path += file_name
-                complete_path = complete_path.replace('//', '/')
-                files_to_process.append(complete_path)
-    else:
-        complete_path = dirname(abspath(file_or_dir)) + '/'
-        complete_path += file_or_dir
-        files_to_process.append(complete_path)
+    for (dirpath, dirnames, filenames) in walk(file_or_dir):
+        for file_name in filenames:
+            complete_path = abspath(dirpath) + '/'
+            complete_path += file_name
+            complete_path = complete_path.replace('//', '/')
+            files_to_process.append(complete_path)
         
     return sorted(files_to_process)
     
@@ -175,20 +158,59 @@ def insert_into_db(conn, file, title = 'Unknown', artist = 'Unknown',
     conn.commit()
     
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option("-u", "--upload", dest="upload",
-                      help="File to upload or folder containing files to "\
-                        "upload.")
-    parser.add_option("-d", "--download", 
-                      help = "Absolute path to download folder.")
+    import os
 
-    (options, args) = parser.parse_args()
-    if options.download and options.upload:
-        print('Download/upload are mutually exclusive.')
+    root_dir = os.getenv('YTFS_ROOT_DIR', None)
+    mount_dir = os.getenv('YTFS_MOUNT_DIR', None)
+    search_dir = os.getenv('YTFS_SEARCH_DIR', None)
+    download_flg = os.getenv('YTFS_DOWNLOAD_FLG', None)
+    upload_flg = os.getenv('YTFS_UPLOAD_FLG', None)
+
+    root_dir = root_dir.strip() if root_dir is not None else root_dir
+    mount_dir = mount_dir.strip() if mount_dir is not None else mount_dir
+    search_dir = search_dir.strip() if search_dir is not None else search_dir
+    download_flg = download_flg.strip() if download_flg is not None else download_flg
+    upload_flg = upload_flg.strip() if upload_flg is not None else upload_flg
+    
+    params = [{'param': 'YTFS_ROOT_DIR', 'is_valid': root_dir is not None \
+                and isdir(root_dir)},
+              {'param': 'YTFS_MOUNT_DIR', 'is_valid': mount_dir is not None \
+                and isdir(mount_dir)},
+              {'param': 'YTFS_SEARCH_DIR', 'is_valid': search_dir is not None \
+                and isdir(search_dir)},
+              {'param': 'YTFS_DOWNLOAD_FLG', 'is_valid': download_flg is not None \
+                and download_flg.lower() in ['true', 't', '1', 'false', 'f', '0']},
+              {'param': 'YTFS_UPLOAD_FLG', 'is_valid': upload_flg is not None \
+                and upload_flg.lower() in ['true', 't', '1', 'false', 'f', '0']}]
+
+    for param in params:
+        if not param['is_valid']:
+            raise Exception('{param} is undefined/invalid. Please define {param} in '\
+                'an .env file. Then type source <.env file> from the command '\
+                'line before running this script.'.format(param = param['param']))
+
+    if download_flg in ['true', 't', '1']:
+        download_flg = True
+    elif download_flg in ['false', 'f', '0']:
+        download_flg = False
+
+    if upload_flg in ['true', 't', '1']:
+        upload_flg = True
+    elif upload_flg in ['false', 'f', '0']:
+        upload_flg = False
+
+    if download_flg is True and upload_flg is True:
+        raise Exception('Download/upload are mutually exclusive. Both cannot be true.')
+
+    if mount_dir == root_dir:
+        raise Exception('MOUNT_DIR and ROOT_DIR must be different.')
         
     retval, errors = run(
-        download = options.download.strip() if options.download else None,
-        upload = options.upload.strip() if options.upload else None
+        root_dir = root_dir,
+        mount_dir = mount_dir,
+        search_dir = search_dir,
+        download_flg = download_flg,
+        upload_flg = upload_flg
     )
     
     if errors:
